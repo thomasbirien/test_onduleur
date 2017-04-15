77c1cf2b909037c4f0af6837a2403fbbc295e2f9
Thomas Birien <thomasbirien@gmail.com>
2017-04-15 18:15:10 +0200

    added 2 methods for separate date and time

--- app/models/onduleur.rb
+++ app/models/onduleur.rb
@@ -5,4 +5,29 @@ class Onduleur < ApplicationRecord
       Onduleur.create! row.to_hash
     end
   end
+
+  def self.date_for_collection
+    onduleurs = Onduleur.all
+    date_time = onduleurs.map {|onduleur| onduleur[:datetime]}
+    date_collection = []
+    date_time.each do |dt|
+      date = DateTime.strptime(dt, "%d/%m/%y %R")
+      date.to_date
+      date_collection << date.strftime("%d/%m/%y")
+    end
+    date_collection.uniq
+  end
+
+  def self.time_for_collection
+    onduleurs = Onduleur.all
+    date_time = onduleurs.map {|onduleur| onduleur[:datetime]}
+    time_collection = []
+    date_time.each do |dt|
+      time = DateTime.strptime(dt, "%d/%m/%y %R")
+      time.to_date
+      time_collection << time.strftime("%R")
+    end
+    time_collection.uniq
+  end
+
 end
