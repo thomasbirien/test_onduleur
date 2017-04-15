122609fc1b1cadc239b21cc7156aae0df8159392
Thomas Birien <thomasbirien@gmail.com>
2017-04-15 20:20:31 +0200

    sum for a specific day is done, sum for a specifiq date and hour is done too. Methods/routes show deleted

--- app/controllers/onduleurs_controller.rb
+++ app/controllers/onduleurs_controller.rb
@@ -1,10 +1,14 @@
 class OnduleursController < ApplicationController
   def index
-    @onduleurs = Onduleur.all
-  end
-
-  def show
-    @onduleur = Onduleur.where(datetime: params[:datetime])
+    if params.has_key?("date")
+      @onduleurs_choosen = Onduleur.where("datetime LIKE '#{params[:date][:date]} %'" )
+      @total_for_a_date = @onduleurs_choosen.sum(:energy)
+    elsif params.has_key?("date_time")
+      @onduleurs_choosen_by_time = Onduleur.where("datetime LIKE '#{params[:date_time][:date]} #{params[:date_time][:time]}'" )
+      @total_for_a_hour = @onduleurs_choosen_by_time.sum(:energy)
+    else
+      @onduleurs = Onduleur.all
+    end
   end
 
   def import
