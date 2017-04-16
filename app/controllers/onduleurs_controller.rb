aa3b67132cd8a3017d7100a759067b3a9de53d5a
Thomas Birien <thomasbirien@gmail.com>
2017-04-16 18:19:33 +0200

    secure for injection SQL and adding onduleur_id for DateForTotal

--- app/controllers/onduleurs_controller.rb
+++ app/controllers/onduleurs_controller.rb
@@ -1,10 +1,10 @@
 class OnduleursController < ApplicationController
   def index
     if params.has_key?("date")
-      @onduleurs_choosen = Onduleur.where("datetime LIKE '#{params[:date][:date]} %'" )
+      @onduleurs_choosen = Onduleur.where("datetime LIKE ?", "#{params[:date][:date]} %" )
       @total_for_a_date = @onduleurs_choosen.sum(:energy)
     elsif params.has_key?("date_time")
-      @onduleurs_choosen_by_time = Onduleur.where("datetime LIKE '#{params[:date_time][:date]} #{params[:date_time][:time]}'" )
+      @onduleurs_choosen_by_time = Onduleur.where("datetime LIKE ?", "#{params[:date_time][:date]} #{params[:date_time][:time]}" )
       @total_for_a_hour = @onduleurs_choosen_by_time.sum(:energy)
     else
       @onduleurs = Onduleur.all
